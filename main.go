ea6cc774df3836511bd9b719d11fcb91a0141a9e
Hin Sinak <hin.sinak@gmail.com>
2022-12-17 15:19:57 +0700

    initialized

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"gograph/graph/generated"
+	"gograph/graph/resolver"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
+
+	http.Handle("/graphql", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# starter graphql generator with GO
